1e5c0a76b28d5d4d0dc8ab0fb68a0475b6784e0d
Oluwadamiloju <yusufdamiloju@gmail.com>
2020-03-19 17:20:00 +0100

    wip

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        brand_purple: '#4A0D83'
+      },
+      width: {
+        '189': '11.813rem'
+      },
+      fontSize: {
+        '33': '2.063rem'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
