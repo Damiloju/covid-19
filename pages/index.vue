9793841487a2f90763c63fb6fdc8cfd63b08fe9a
Oluwadamiloju <yusufdamiloju@gmail.com>
2020-03-20 14:32:16 +0100

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,146 @@
 <template>
   <div class="w-full h-screen flex">
-    <div class="w-7/12 h-full">
-      <div class="w-189 h-12 my-auto text-white ml-8 mt-8 bg-brand_purple">
-        <span class="my-2 mx-4 font-bold text-33 text-left">
-          COVID-19
-        </span>
-      </div>
-    </div>
+    <div class="w-full grid sm:grid-cols-1 md:grid-cols-2 gap-0">
+      <div class="h-full bg-white">
+        <div class="w-189 h-12 my-auto text-white ml-8 mt-8 bg-brand_purple">
+          <span class="my-2 mx-4 font-bold text-33 text-left">
+            COVID-19
+          </span>
+        </div>
 
-    <div class="w-auto h-full bg-red-500">
-      Hello
+        <span class="">
+          <img
+            src="~/assets/images/woman_smaller.png"
+            class="relative md:absolute top-207 left-141 object-cover"
+            alt=""
+          />
+          <img
+            src="~/assets/images/countries.png"
+            class="relative md:absolute top-181 left-292 w-553 h-300"
+            alt=""
+          />
+        </span>
+        <div
+          class="mt-500 ml-141 flex flex-col md:flex-row justify-between mr-24"
+        >
+          <div
+            class="cards transition duration-300 ease-in-out transform hover:-translate-y-6 w-198 h-236 rounded-lg shadow-md border border-brand_purple text-brand_purple hover:bg-brand_purple hover:text-white cursor-pointer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 68 79.854"
+              class="w-68 h-20 fill-current mx-auto mt-10"
+            >
+              <defs />
+              <path
+                class="a"
+                d="M33.999 24.714a15.215 15.215 0 1015.215 15.214 15.232 15.232 0 00-15.215-15.214zm9.033 12.349l-10.9 10.9a2.342 2.342 0 01-3.314 0l-4.165-4.165a2.343 2.343 0 113.313-3.313l2.509 2.509 9.244-9.243a2.343 2.343 0 013.313 3.313zm0 0"
+              />
+              <path
+                class="a"
+                d="M67.921 21.666v-.062a82.315 82.315 0 01-.072-2.481 8.463 8.463 0 00-7.969-8.291c-9.18-.513-16.286-3.506-22.354-9.421l-.052-.049a5.11 5.11 0 00-6.945 0l-.052.049c-6.069 5.915-13.171 8.909-22.352 9.422a8.463 8.463 0 00-7.969 8.291 79.87 79.87 0 01-.072 2.481v.144c-.179 9.362-.4 21.014 3.5 31.59a40.212 40.212 0 009.647 15.027A50.467 50.467 0 0032.1 79.532a5.84 5.84 0 00.774.211 5.716 5.716 0 002.247 0 5.844 5.844 0 00.777-.212A50.5 50.5 0 0054.76 68.358a40.271 40.271 0 009.647-15.031C68.32 42.72 68.1 31.046 67.921 21.666zM34 59.829a19.9 19.9 0 1119.9-19.9 19.923 19.923 0 01-19.9 19.9zm0 0"
+              />
+            </svg>
+
+            <div class="mx-6 mt-12">
+              <hr class="border-t border-brand_purple" />
+            </div>
+
+            <p class="mt-4 font-bold text-center">Symptoms</p>
+          </div>
+          <div
+            class="cards transition duration-300 ease-in-out transform hover:-translate-y-6 w-198 h-236 rounded-lg shadow-md border border-brand_purple text-brand_purple hover:bg-brand_purple hover:text-white cursor-pointer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 75.095 73.839"
+              class="w-68 h-20 fill-current mx-auto mt-10"
+            >
+              <defs />
+              <path
+                class="a"
+                d="M75.074 33.781a6.457 6.457 0 01-5.574 7.036 5.946 5.946 0 01-5.96-3.5l-3.042.268-2.3 1.108a1.734 1.734 0 00-.959 1.331 20.258 20.258 0 01-1.158 4.474 1.726 1.726 0 00.1 1.469L57 47.428l2.275 1.527a4.091 4.091 0 11-2.585 4.384l-2.521-1.692-1.587-.194a1.827 1.827 0 00-1.485.5 20.536 20.536 0 01-4.5 3.248 1.805 1.805 0 00-.947 1.485l-.2 3.214 1 3.96a5.082 5.082 0 11-4.92 1.267l-1-3.983-1.548-2.489a2.267 2.267 0 00-1.937-1.062h-.012a20.521 20.521 0 01-3.966-.387 2.264 2.264 0 00-2.089.656l-2.046 2.159-2.291 4.752a4.09 4.09 0 11-4.592-2.176l2.3-4.783.453-3.21a1.705 1.705 0 00-.609-1.564 20.6 20.6 0 01-4.634-5.311 1.7 1.7 0 00-1.443-.814l-3.2-.031-3.509 1.1a5.722 5.722 0 11-1.74-4.778l3.724-1.172 2.72-1.939a1.4 1.4 0 00.58-1.26q-.067-.821-.068-1.657a20.286 20.286 0 011.857-8.5 1.4 1.4 0 00-.014-1.192l-.81-1.639-2.187-1.65a4.091 4.091 0 112.824-4.234l2.424 1.829 2.442.435a20.459 20.459 0 016.126-3.926l.573-3.484-.724-3.6a5.865 5.865 0 01-4.349-4.381A5.731 5.731 0 0129.532.112a6.058 6.058 0 017.138 4.472 5.563 5.563 0 01-2.514 5.836l.683 3.394 1.678 2.678a.6.6 0 00.508.281 20.272 20.272 0 0110.515 2.926.592.592 0 00.555.031l2.628-1.218 3.069-3.32a4.09 4.09 0 113.73 3.45l-3.069 3.32-1 2.681a.593.593 0 00.077.555 20.3 20.3 0 012.842 5.5 1.743 1.743 0 001.173 1.127l2.5.7 3.242-.287a6.267 6.267 0 015.218-4.271 6.107 6.107 0 016.565 5.818z"
+              />
+              <circle
+                class="b"
+                cx="15.113"
+                cy="15.113"
+                r="15.113"
+                transform="translate(21.916 22.07)"
+              />
+              <path
+                class="b"
+                d="M26.522 37.183a15.117 15.117 0 0112.81-14.939 15.113 15.113 0 100 29.878 15.117 15.117 0 01-12.81-14.939z"
+              />
+              <g transform="translate(7.297 1.1)">
+                <circle class="a" cx="4.304" cy="4.304" r="4.304" />
+                <circle
+                  class="a"
+                  cx="2.843"
+                  cy="2.843"
+                  r="2.843"
+                  transform="translate(.285 58.192)"
+                />
+                <circle
+                  class="a"
+                  cx="4.187"
+                  cy="4.187"
+                  r="4.187"
+                  transform="translate(51.134 61.517)"
+                />
+                <circle
+                  class="a"
+                  cx="3.904"
+                  cy="3.904"
+                  r="3.904"
+                  transform="translate(21.19 28.012)"
+                />
+                <circle
+                  class="a"
+                  cx="2.657"
+                  cy="2.657"
+                  r="2.657"
+                  transform="translate(25.57 41.383)"
+                />
+                <circle
+                  class="a"
+                  cx="2.231"
+                  cy="2.231"
+                  r="2.231"
+                  transform="translate(34.339 33.852)"
+                />
+              </g>
+            </svg>
+            <div class="mx-6 mt-12">
+              <hr class="border-t border-brand_purple" />
+            </div>
+
+            <p class="mt-4 font-bold text-center">What is Covid-19</p>
+          </div>
+          <div
+            class="cards transition duration-300 ease-in-out transform hover:-translate-y-6 w-198 h-236 rounded-lg shadow-md border border-brand_purple text-brand_purple hover:bg-brand_purple hover:text-white cursor-pointer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 69 69"
+              class="w-68 h-20 fill-current mx-auto mt-10"
+            >
+              <path
+                d="M34.5 0A34.5 34.5 0 1069 34.5 34.54 34.54 0 0034.5 0zm0 63.25A28.645 28.645 0 0114.375 55v-4.688a5.408 5.408 0 011.32-3.267 6.659 6.659 0 001.555-3.921c0-2.742-2.475-3.02-3.806-3.168a4 4 0 01-2.365-.723 9.094 9.094 0 01-1.193-1.849 11.082 11.082 0 00-4.045-4.67A28.763 28.763 0 0134.5 5.75c.789 0 1.565.056 2.338.118a2.912 2.912 0 00-.479.3 1.436 1.436 0 00.06 2.09 3.838 3.838 0 01.636.691c-.032.069-.3.519-1.767 1.266-2.429 1.231-5.233 3.751-5.65 6.373a3.5 3.5 0 00.971 3.112 1.436 1.436 0 001.016.421 11.216 11.216 0 005.031-1.452 10.915 10.915 0 015.031-1.424c5.642 0 10.063 1.263 10.063 2.875 0 .591-.184.772-.254.839-.706.695-3.111.65-5.225.618-.56-.01-1.133-.02-1.708-.02a6.225 6.225 0 01-3.257-.616 7.85 7.85 0 00-3.931-.821c-2.37 0-6.773.428-9.641 3.3-2.346 2.344-2.1 5.149-1.934 7 .038.427.076.831.076 1.2 0 2.831 2.893 4.312 5.75 4.312 4.394 0 8.193.82 8.625 1.438a8.325 8.325 0 00.863 3.589 5.015 5.015 0 01.574 2.16 1.436 1.436 0 01-.394 1.161 4.524 4.524 0 00-1.043 3.151c0 3.025 2.913 6.344 3.244 6.713a1.44 1.44 0 001.068.475 1.4 1.4 0 00.357-.045c.846-.218 8.268-2.26 8.268-7.143a2.738 2.738 0 011.2-2.569 3.922 3.922 0 001.678-3.181c0-1.153 2.045-4.127 3.941-6.223a1.437 1.437 0 00-.184-2.1c-1.117-.866-4.2-3.686-5.236-6.422a8.221 8.221 0 011.9 1.387 2.394 2.394 0 001.85.682 7.482 7.482 0 003.858-2.134A28.657 28.657 0 0134.5 63.25z"
+              />
+            </svg>
+            <div class="mx-6 mt-12">
+              <hr class="border-t border-brand_purple" />
+            </div>
+
+            <p class="mt-4 font-bold text-center">Countries Affected</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="h-full w-full">
+        <div class="w-11/12 h-full gradient-color ml-auto"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -18,4 +149,22 @@
 export default {}
 </script>
 
-<style></style>
+<style>
+.gradient-color {
+  background: rgb(223, 40, 0);
+  background: linear-gradient(rgba(223, 40, 0, 1) 0%, rgba(250, 98, 34, 1) 0%);
+}
+
+.cards:hover {
+  background: rgb(93, 0, 130);
+  background: linear-gradient(
+    142deg,
+    rgba(93, 0, 130, 1) 23%,
+    rgba(48, 0, 67, 1) 55%
+  );
+}
+
+.cards:hover hr {
+  border-top: 1px solid white;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,35 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand_purple: '#4A0D83'
+        brand_purple: '#4A0D83',
+        brand_orange: '#f35915'
       },
       width: {
-        '189': '11.813rem'
+        '189': '11.813rem',
+        '553': '34.563rem',
+        '198': '12.375rem',
+        '68': '4.25rem'
+      },
+      height: {
+        '300': '18.75rem',
+        '236': '14.75rem'
       },
       fontSize: {
         '33': '2.063rem'
+      },
+      fontFamily: {
+        montserrat: ['Montserrat'],
+        poppins: ['Poppins']
+      },
+      inset: {
+        '181': '11.313rem',
+        '292': '18.25rem',
+        '141': '8.813rem',
+        '207': '12.938rem'
+      },
+      margin: {
+        '500': '31.25rem',
+        '141': '8.813rem'
       }
     }
   },
